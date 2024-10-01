966358202c86e7f370192bdb9a2b5992f40987a8
ttsamanemashaba004 <ttsamane.mashaba@gmail.com>
2024-10-01 17:38:42 +0200

    Completed the function for computer choice.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-console.log('Hello World')
+function getComputerChoice(){
+    const rand = Math.random();
+    if(rand < 0.4){
+        return 'rock';
+    }
+    else if(rand > 0.4 && rand < 0.7){
+        return 'paper'
+    }
+    else{
+        return 'scissors'
+    }
+}
+
+
+console.log(getComputerChoice())
+
